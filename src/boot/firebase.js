2a4228d078ce704d625b9bc4977122da131a42c5
Roberto Scarciello <rscarciello@gmail.com>
2021-11-08 17:43:59 +0100

    Aggiornato il file firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,7 +1,43 @@
+/* eslint-disable */
 import { boot } from 'quasar/wrappers'
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-  // something to do
+const firebaseConfig = {
+  apiKey: "XXXXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXX",
+  appId: "XXXX",
+  measurementId: "G-XXXXX"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
+ firebase.getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+      const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+        unsubscribe();
+        resolve(user);
+      }, reject);
+    })
+  };
+
+export default boot(({ app, router, store }) => {
+  app.config.globalProperties.$firebase = firebase;
+  app.config.globalProperties.$analytics = analytics;
+
+  router.beforeEach(async (to, from, next) => {
+    const auth = to.meta.requiresAuth
+    if (auth && !await firebase.getCurrentUser()) {
+      next('/');
+    } else {
+      next();
+    }
+  })
 })
+
+export { firebase, analytics };
+
